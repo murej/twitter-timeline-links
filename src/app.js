b6f66f04afd6993b52233d7d4cde76ad967aa4ad
Jure Martinec <jure.martinec@gmail.com>
2019-07-26 16:02:47 +0200

    Set up Twitter client

--- src/app.js
+++ src/app.js
@@ -3,11 +3,35 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import twitterClient from 'twitter-node-client';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
 const app = express();
+const Twitter = twitterClient.Twitter;
+
+const config = {
+	"consumerKey": process.env.TWITTER_CONSUMER_KEY,
+	"consumerSecret": process.env.TWITTER_CONSUMER_SECRET,
+	"accessToken": process.env.TWITTER_ACCESS_TOKEN,
+	"accessTokenSecret": process.env.TWITTER_ACCESS_TOKEN_SECRET
+}
+const twitter = new Twitter(config);
+
+const onError = (err, response, body) => {
+  console.log(err);
+}
+const onSuccess = (entries) => {
+  JSON.parse(entries).forEach((entry) => {
+    const urls = entry.entities.urls;
+    const hasUrls = urls.length > 0;
+    hasUrls && console.log(urls[0].expanded_url);
+  });
+}
+
+twitter.getHomeTimeline({ screen_name: 'youremartinets', count: '200'}, onError, onSuccess);
+
 
 app.use(logger('dev'));
 app.use(express.json());
